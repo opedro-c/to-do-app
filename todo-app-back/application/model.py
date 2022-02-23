e1a735d1395764fc140ad9cf40e1625d686f5d6f
Pedro Costa <pedroc_aragao@outlook.com>
2022-02-23 15:04:37 -0300

    date added to task entity

--- todo-app-back/application/model.py
+++ todo-app-back/application/model.py
@@ -14,11 +14,12 @@ class Task(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
     name = DB.Column(DB.String(255), nullable=False)
     description = DB.Column(DB.String(255), nullable=True)
-    # date = DB.Column(DB.Date, nullable=False)
+    date = DB.Column(DB.Date, nullable=False)
     done = DB.Column(DB.Boolean, default=False)
 
-    def __init__(self, task):
+    def __init__(self, task: dict):
         self.id = task.get('id')
         self.name = task.get('name')
         self.description = task.get('description')
+        self.date = task.get('date')
         self.done = task.get('done')
