96236b6532eb16dab51d898048f044d3de023b5f
Leonardo Aragão <lewoaragao@gmail.com>
2022-03-19 19:51:20 -0300

    criação de mais páginas e modo noturno

--- todo-app-front/horaedatadosistema/script.js
+++
@@ -1,6 +0,0 @@
-// INDEX TIME AND DATE
-let datehour = new Date();
-console.log(datehour)
-// let time = document.querySelector('.time')
-// let date = document.querySelector('.date')
-
--- todo-app-front/script.js
+++ todo-app-front/script.js
@@ -1,124 +1,60 @@
-const taskInput = document.querySelector('.task-input input')
-filters = document.querySelectorAll('.filters span')
-clearAll = document.querySelector('.clear-btn')
-taskBox = document.querySelector('.task-box')
-let editId
-let isEditedTask = false
-// getting localStorage todo-list
-let todos = JSON.parse(localStorage.getItem("todo-list"));
+// switch mode
+// gerais
+let body = document.querySelector('body')
+let h1 = document.querySelector('h1')
+let a = document.querySelectorAll('a')
 
-filters.forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.querySelector("span.active").classList.remove("active")
-        btn.classList.add('active')
-        showTodo(btn.id)
-    })
-})
+// especificos
+let btnSwitchMode = document.querySelector(".btnSwitchMode")
+let iSwitchMode = document.querySelector(".iSwitchMode")
+let wrapper = document.querySelector('.wrapper')
+let controls = document.querySelector('.filters')
+let btnClearAll = document.querySelector('.clear-btn')
+let spanActive = document.querySelector('span.active')
 
-function showTodo(filter) {
-    let li = ''
-    if(todos) {
-        todos.forEach((todo, id) => {
-            // if todo status is completed, set the isCompleted value to checked
-            let isCompleted = todo.status == 'completed' ? 'checked' : '';
-
-            if(filter == todo.status || filter == 'all') {
-                li += `<li class="task">
-            <label for="${id}">
-                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted}>
-                <p class="${isCompleted}">${todo.name}</p>
-            </label>
+btnSwitchMode.addEventListener('click', () => {
+    const darkMode = iSwitchMode.classList.toggle('uil-sun')
     
-            <div class="settings">
-                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i> <ul class="task-menu">
-                    <li onclick="editTask(${id}, '${todo.name}')"><i class="uil uil-pen">Edit</i> </li>
-                    <li onclick="deleteTask(${id})"><i class="uil uil-trash">Delete</i> </li>
-                </ul>
-            </div>
-        </li>`
-            }
+    if(darkMode) {
+        body.style.background = 'var(--invert-main-collor)'
+        h1.style.color = 'white'
 
-            
-        })
-    }
+        wrapper.style.background = 'slategray'
+        wrapper.style.boxShadow  = '4px 4px rgba(0, 0, 0, 0.25)'
+        controls.style.color = 'white'
+        btnClearAll.style.background = 'var(--invert-main-collor)'
 
-    // if li isn't insert this value inside taskbox else insert span
-    taskBox.innerHTML = li || `<span style="color: var(--color-border)">Você não tem nenhuma tarefa aqui.</span>`
-}
-showTodo('all')
-
-function showMenu(selectedTask) {
-    // getting task menu div
-    let taskMenu = selectedTask.parentElement.lastElementChild
-    taskMenu.classList.add('show')
-    document.addEventListener('click', e => {
-        // removing show class from the task menu on the document click
-        if(e.target.tagName != 'I' || e.target != selectedTask) {
-            taskMenu.classList.remove('show')
-        }
-    })
-}
-
-function editTask(taskId, taskName) {
-    editId = taskId
-    isEditedTask = true
-    taskInput.value = taskName
-    taskInput.focus()
-}
-
-function deleteTask(deleteId) {
-    // removing selected task from array/todos
-    todos.splice(deleteId, 1)
-    localStorage.setItem('todo-list', JSON.stringify(todos))
-    showTodo('all')
-}
-
-clearAll.addEventListener('click', () => {
-    // removing all items of array/todos
-    todos.splice(0, todos.length)
-    localStorage.setItem('todo-list', JSON.stringify(todos))
-    showTodo('all')
-})
-
-function updateStatus(selectedTask) {
-    // getting paragraph that contains task name
-    let taskName = selectedTask.parentElement.lastElementChild
-
-    if(selectedTask.checked) {
-        taskName.classList.add('checked')
-        // updating the status of selected task to completed
-        todos[selectedTask.id].status = 'completed'
     } else {
-        taskName.classList.remove('checked')
-        // updating the status of selected task to pending
-        todos[selectedTask.id].status = 'pending'
-    }
-    
-    localStorage.setItem('todo-list', JSON.stringify(todos))
-}
+        body.style.background = ''
+        h1.style.color = ''
 
-taskInput.addEventListener('keyup', e => {
-    let userTask = taskInput.value.trim()
-
-    if (e.key == 'Enter' && userTask) {
-
-        if(!isEditedTask) { // if isEditedTask isn't true
-            if (!todos) { // if todos isn't exist, pass an empty array to todos
-                todos = [];
-            }
-
-            let taskInfo = {
-                name: userTask,
-                status: 'pending'
-            }
-            todos.push(taskInfo)  // adding new task to todos
-        } else {
-            isEditedTask = false
-            todos[editId].name = userTask
-        }
-
-        taskInput.value = ''
-        localStorage.setItem('todo-list', JSON.stringify(todos))
-        showTodo('all')  
+        wrapper.style.background = ''
+        wrapper.style.boxShadow = ''
+        controls.style.color = ''
+        btnClearAll.style.background = ''
     }
 })
+
+// system time and date
+function relogio(){
+    var data = new Date();
+    var hor = data.getHours();
+    var min = data.getMinutes();
+    var seg = data.getSeconds();
+    
+    if(hor < 10){
+        hor = "0" + hor;
+    }
+    if(min < 10){
+        min = "0" + min;
+    }
+    if(seg < 10){
+        seg = "0" + seg;
+    }
+    
+    var horas = hor + ":" + min + ":" + seg;
+    document.getElementById("rel").value = horas;
+}
+
+var timer = setInterval(relogio);
+
